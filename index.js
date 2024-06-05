5f32888d2b4b22208765d6f76ef6ee6106ffab96
Finley <finley.mwachia12@gmail.com>
2024-06-05 15:04:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-
+var nameLength = ""
+var topText = "Write your name below"
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs",{topText})
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  nameLength = req.body["fName"].length + req.body["lName"].length;
+  topText = `Your name has  ${nameLength} Characters`;
+  res.render("index.ejs",{topText})
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
